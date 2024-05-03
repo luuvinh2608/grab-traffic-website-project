727fc780f543130cced5c4a42393f10e01861839
Lưu Quốc Vinh <42367441+Vinh2608@users.noreply.github.com>
2024-05-03 03:50:09 -0300

    Update database.py by removing uri

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,6 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://vinhluu2608:vuongtranlinhlinh123456789@cluster0.teog563.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
