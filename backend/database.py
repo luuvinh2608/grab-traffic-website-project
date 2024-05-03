8b572727c2a40a5b175c74ab4484fb8822ad831c
Vinh2608 <vinhluu1996@gmail.com>
2024-05-03 06:04:04 -0300

    remove the uri

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,13 @@
-
+import requests
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-
-uri = "mongodb+srv://vinhluu2608:vuongtranlinhlinh123456789@cluster0.teog563.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['grab-engineering-project']
 collection = db['Place_LatLong_API']
+
+air_quality_link = ''
+url = requests.get('')
+
+
