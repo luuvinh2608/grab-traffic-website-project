991dc65b2799243fdb9a2ca675d0d1e54e7462f5
Vinh2608 <vinhluu1996@gmail.com>
2024-05-05 12:50:59 +0700

    connect database

--- backend/database.py
+++ backend/database.py
@@ -2,12 +2,19 @@ import requests
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-db = client['grab-engineering-project']
-collection = db['Place_LatLong_API']
+import json
 
-air_quality_link = ''
-url = requests.get('')
+uri = f"mongodb+srv://vinhluu2608:vuongtranlinhlinh123456789@cluster0.teog563.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+CLIENT = MongoClient(uri, server_api=ServerApi('1'))
+# CLIENT['grab-engineering-project']['Place_LatLong_API'].find_one_and_update({'id': 1},{"$unset": {'traffic_data': 1}})
+
+#CLIENT['grab-engineering-project']['Place_LatLong_API'].find_one_and_update({'id': 1},{"$unset": {'traffic_data': 1}})
+for i in range(2, 127):
+    CLIENT['grab-engineering-project']['Place_LatLong_API'].find_one_and_update(
+        {'id': i}, {"$unset": {'air_data': 1}})
+
+
 
 
